8ab3e26f6b9d7f718e511cd360bc1d2831a58530
aakashloyar <aakashloyar@gmail.com>
2025-03-12 18:09:31 +0530

    added session jwt in auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,5 +2,36 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  session: {
+    strategy: "jwt", 
+    maxAge: 24 * 60 * 60, 
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      console.log("jwt Start ******")
+      console.log(token);
+      console.log('Gap');
+      console.log(user);
+      console.log("jwt end");
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      console.log('session start****')
+      console.log(session);
+      console.log('Gap');
+      console.log(token);
+      console.log("session end");
+      if(token.id) {
+        session.user = {
+          ...session.user,
+          id: token.id as string,
+        };
+      }
+      return session;
+    },
+  },
   secret:process.env.NEXT_AUTH_SECRET
 })
