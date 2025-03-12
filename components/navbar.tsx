7b6fea2d48477b897881eb5d5680d582ab901638
aakashloyar <aakashloyar@gmail.com>
2025-03-12 11:35:57 +0530

    created a navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,27 @@
+"use client"
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import {Button} from '@/components/ui/button'
+export default function() {
+    const router = useRouter();
+    function handleLog() {
+        router.push('/signin');
+    }
+    function NeuraaiClick() {
+        router.push('/');
+    }
+    return (
+        <div className='w-full h-14 bg-slate-200 flex justify-between items-center'>
+            <div className='pl-2 font-bold'>
+                <Link href="/" className="text-xl cursor-pointer">
+                   NeuraAI
+                </Link>
+            </div>
+            <div className='pr-2'>
+                <Button variant={"default"} size={"lg"} className='bg-slate-900 hover:bg-slate-600 text-xl font-semibold ' onClick={handleLog}>
+                   Login
+                </Button>
+            </div>
+        </div>
+    )
+}
