1c95c429ba36a6884962a16ec5ca6c59af4cf8d0
aakashloyar <aakashloyar@gmail.com>
2025-03-12 12:45:15 +0530

    added footer to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
-import {Button} from '@/components/ui/button'
+"use client"
+import { Sidebar } from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+
   return (
-    <div className='pt-2 px-2'>
+    <div className="">
+      <div className="p-4 flex-1">
+        <p>Main Content Here</p>
+      </div>
+      <footer className="fixed bottom-0 left-0 w-full bg-slate-100 text-slate-600 text-center py-2">
+        © NeuraGPT can make mistakes | All rights reserved.
+      </footer>
     </div>
-    
   );
 }
