163901a31311e9cb81207959f462deb195016365
aakashloyar <aakashloyar@gmail.com>
2025-03-12 18:13:32 +0530

    added myaccount to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,6 @@
-"use client"
-import { useRouter } from "next/navigation";
-import Link from "next/link";
-import {Button} from '@/components/ui/button'
-export default function() {
-    const router = useRouter();
-    function handleLog() {
-        router.push('api/auth/signin');
-    }
+import Link from "next/link"
+import {Dropdown} from "@/components/dropdown"
+export default function() {    
     return (
         <div className='w-full h-14 bg-slate-200 flex justify-between items-center'>
             <div className='pl-2 font-bold'>
@@ -15,9 +9,7 @@ export default function() {
                 </Link>
             </div>
             <div className='pr-2'>
-                <Button variant={"default"} size={"lg"} className='bg-slate-900 hover:bg-slate-600 text-xl font-semibold ' onClick={handleLog}>
-                   Login
-                </Button>
+                <Dropdown/>
             </div>
         </div>
     )
