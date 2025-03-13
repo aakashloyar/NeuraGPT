b48e59cf1bf97a8955051010bed64543a11430bb
aakashloyar <aakashloyar@gmail.com>
2025-03-13 13:06:52 +0530

    created route for chats

---
+++ app/api/chat/new/route.ts
@@ -0,0 +1,22 @@
+import {auth} from '@/lib/auth'
+import { NextRequest,NextResponse } from "next/server";
+import {prisma} from "@/lib/primsa"
+export async function POST(req:NextRequest) {
+    try{
+        const session=await auth();
+        const userId=session?.user?.id||"";
+        const chat=await prisma.chat.create({
+            data: {
+                userId:userId
+            }
+        })
+        return NextResponse.json({message:chat.id},{status:200});
+    } catch(err) {
+        return NextResponse.json({error:err},{status:500})
+    }
+    
+}
+
+export async function GET(req:NextRequest) {
+    return NextResponse.json({message:"hello world"},{status:200})
+}
