933e6af84fd8b924221c1203bde44c926ac88640
aakashloyar <aakashloyar@gmail.com>
2025-03-13 12:59:42 +0530

    shifted text area from page to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-import { Sidebar } from "@/components/ui/sidebar";
 import {Textarea} from "@/components/textarea"
 export default function Home() {
 
   return (
     <div className="">
-      <div className="">
-        <p>Main Content Here</p>
-      </div>
-      <footer className="fixed bottom-0 w-full">
-        <div className='flex justify-center'>
-          <Textarea/>
-        </div>
-        <div className='bg-slate-100 text-sm text-slate-600 text-center py-2'>
-          © NeuraGPT can make mistakes | All rights reserved.
-        </div>
-      </footer>
+      
     </div>
   );
 }
