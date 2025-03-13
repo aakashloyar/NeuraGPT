4e7c87d01a50958e64187d0a63edbd7ee39927f2
aakashloyar <aakashloyar@gmail.com>
2025-03-13 13:05:12 +0530

    added content to sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,20 +1,75 @@
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarHeader,
+  Sidebar,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
   } from "@/components/ui/sidebar"
-  
+  const items = [
+    {
+      title: "Home",
+      url: "#",
+    },
+    {
+      title: "Inbox",
+      url: "#",
+    },
+    {
+      title: "Calendar",
+      url: "#",
+    },
+    {
+      title: "Search",
+      url: "#",
+    },
+    {
+      title: "Settings",
+      url: "#",
+    },
+  ]
   export function AppSidebar() {
     return (
       <Sidebar>
-        <SidebarHeader />
+        <SidebarHeader>
+          <div className="text-xl text-slate-900 font-semibold">
+            Chat History
+          </div>
+        </SidebarHeader>
         <SidebarContent>
           <SidebarGroup />
+            <SidebarGroupLabel>Application</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+
           <SidebarGroup />
         </SidebarContent>
-        <SidebarFooter />
+        <SidebarFooter>
+          <div className="bg-slate-200">
+            <div className=''>
+              Parent Company
+            </div>
+            <div className='text-xl text-slate-900'>
+              NeuraAI
+            </div>
+          </div>
+        
+        </SidebarFooter>
       </Sidebar>
     )
   }
