26fa2159d28a0b1fd83a592603e922ac88c33fbb
aakashloyar <aakashloyar@gmail.com>
2025-03-16 16:07:10 +0530

    fixed navbar padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,9 +53,9 @@ export default async function RootLayout({
         <SidebarProvider>
           <AppSidebar initChats={chats||[]}/>
           <main className="flex flex-col w-full">
-            <div className='sticky w-full'>
+            <div className='sticky top-0 w-full'>
                <Navbar/>
-              </div>
+            </div>
             <div className="mb-52" > {children}</div>
             <footer className="fixed bottom-0 w-full z-40 bg-white">
             <hr className="border-t-1 border-gray-400 mb-2 mt-1 mx-4"  />
