e3a5415d48012e96ce38fbdea500c17972c9e20d
aakashloyar <aakashloyar@gmail.com>
2025-03-16 18:18:05 +0530

    added history validation

--- lib/validation.ts
+++ lib/validation.ts
@@ -10,6 +10,11 @@ export const messageSchema=z.object({
     answer:z.string(),
     createdAt: z.date(),
 })
+export const HistorySchema=z.object({
+    question:z.string(),
+    answer:z.string()
+})
 
 export type ChatType = z.infer<typeof chatSchema>;
 export type MessageType = z.infer<typeof messageSchema>;
+export type History = z.infer<typeof HistorySchema>;
