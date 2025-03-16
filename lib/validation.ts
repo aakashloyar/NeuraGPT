46e75b029117fe8ca18d7accb97900f9c02302de
aakashloyar <aakashloyar@gmail.com>
2025-03-16 10:59:26 +0530

    added zod validation for messages

--- lib/validation.ts
+++ lib/validation.ts
@@ -4,5 +4,12 @@ export const chatSchema=z.object({
     title: z.string(),
     createdAt: z.date(),
 })
+export const messageSchema=z.object({
+    id: z.string(),
+    question: z.string(),
+    answer:z.string(),
+    createdAt: z.date(),
+})
 
 export type ChatType = z.infer<typeof chatSchema>;
+export type MessageType = z.infer<typeof messageSchema>;
